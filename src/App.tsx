12acb6fdfc02fb4f06aa250d63b529cb899dd164
GiovannaNaves <110492330+GiovannaNaves@users.noreply.github.com>
2023-03-05 18:15:36 -0300

    Definição de temas do mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
+
 import { Login } from './pages/Login'
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { ptBR } from "@mui/material/locale"
 
 function App() {
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#F9A006"
+      },
+      secondary: {
+        main: "#E60882"
+      },
+      background: {
+        default: "#F0F0F0"
+      },
+      contrastThreshold: 2
+    },
+    typography: {
+      fontFamily: [
+        "Poppins",
+        "sans-serif"
+      ].join(",")
+    },
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 900,
+        md: 900,
+        lg: 1200,
+        xl: 1536
+      }
+    }
+  }, ptBR);
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Login />
-    </div>
+    </ThemeProvider>
   )
 }
 
